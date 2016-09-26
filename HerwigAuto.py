55ae808ae1c78f3d2811309a275a3d78d7f931ac
Ludovic Michel Scyboz <lscyboz@mppui2.t2.rzg.mpg.de>
2016-09-26 09:38:36 +0200

    Reinitialized git repository, first commit.

---
+++ HerwigAuto.py
@@ -0,0 +1,162 @@
+
+#! /usr/bin/python
+
+import os, time
+import sys
+import fileinput
+import glob
+import subprocess
+
+nEvPerFile = 5000
+nRuns = 400
+
+fUser = os.getenv("USER")
+SettingsFolder    = "/afs/ipp-garching.mpg.de/home/l/lscyboz/Settings/"
+SetupFileNameGen    = "setupfile.in"
+WorkFolder	  = "/afs/ipp-garching.mpg.de/home/l/lscyboz/"
+
+flag=False
+
+def SubDirPath (d):
+    return filter(os.path.isdir, [os.path.join(d,f) for f in os.listdir(d)])
+
+def printSetupLinesInSubmitFileRivet(file):
+
+#    setupLines = ["export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/ \n",
+#                  "source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh \n",
+#                  "asetup 20.8.2 \n"
+#                  ]
+
+    setupLines = ["source /afs/ipp-garching.mpg.de/home/l/lscyboz/Herwig-7.0.3/bin/activate\n",
+		  "source /afs/ipp-garching.mpg.de/home/l/lscyboz/Herwig-7.0.3/src/Rivet-2.4.0/rivetenv.sh\n",
+		  "export RIVET_ANALYSIS_PATH=/afs/ipp-garching.mpg.de/home/l/lscyboz/RivetCustomAnalyses/:$RIVET_ANALYSIS_PATH\n"]
+
+    file.writelines(setupLines)
+
+def SubmitHerwigJob(nEvents, seed, InputFileNameGen, index):
+
+
+    specStr          = '%03.0f' % (seed,)
+#    tmpFolder        = sampledPars+specStr+"/"
+    OutputFile       = "seed_"+specStr+".hepmc"
+    OutputFolder     = sampledPars+specStr+"/"
+    OutputYoda        = OutputFolder+"seed_"+specStr+"_"
+    tmp		     = "/tmp/lscyboz/"+settings+"_"+specStr+"/"
+
+#    os.system("mkdir -p "+OutputFolder)
+
+    OutputFileFinal  = OutputFolder+OutputFile
+
+    submitFileNameSH = WorkFolder+"Submit_"+settings+"_"+specStr+".sh"
+
+    flag = False
+    redo = False
+    for norms in options[index+1].split("\t"):
+      if os.path.exists(OutputYoda+norms+".yoda"):
+	f=open(OutputYoda+norms+".yoda",'r')
+	for i, line in enumerate(f):
+	  if "END YODA_COUNTER" in line:
+		number=i
+	f=open(OutputYoda+norms+".yoda",'r')
+	string=f.readlines()[number-1]
+	nE=float(string.split("\t")[2].split("\n")[0])
+	if nE!=nEvents:
+	  redo = True
+      else: redo = True
+    os.chdir(OutputFolder)
+    if len(glob.glob('*.yoda')) <= len(options[index+1].split("\t")):
+	redo = True
+      
+    if not os.path.exists(OutputYoda+options[index+1].split("\t")[0]+".yoda") or redo==True:
+
+	flag=True
+
+        submitfile2 = open(submitFileNameSH, "w")
+        printSetupLinesInSubmitFileRivet(submitfile2)
+
+        codeLines2 = []
+#	codeLines2.append("mkdir -p "+tmpFolder)
+	codeLines2.append("cd "+InputFolder)
+	codeLines2.append("mkdir -p "+tmp)
+	codeLines2.append("cp "+SettingsFolder+SetupFileNameGen+" "+OutputFolder)
+	codeLines2.append("echo 'set /Herwig/Generators/EventGenerator:RandomNumberGenerator:Seed "+str(seed)+"' >> "+OutputFolder+SetupFileNameGen)
+	codeLines2.append("echo 'set /Herwig/Analysis/HepMCFile:Filename "+tmp+OutputFile+"' >> "+OutputFolder+SetupFileNameGen)
+
+	codeLines2.append("Herwig run "+InputFileNameGen+" -N "+str(nEvents)+" -x "+OutputFolder+SetupFileNameGen)
+
+	analyses=""
+	for routines in options[index].split("\t"):
+		analyses += " -a "+routines
+        for norms in options[index+1].split("\t"):
+                codeLines2.append("rivet"+analyses+" "+tmp+OutputFile+" -H "+OutputYoda+norms+".yoda -x "+norms) 
+	codeLines2.append("rivet"+analyses+" "+tmp+OutputFile+" -H "+OutputYoda+"unnorm.yoda")
+
+#        codeLines2.append("cp "+tmpFolder+OutputFile+" "+OutputFileFinal)
+#	codeLines2.append("cp "+tmpFolder+SetupFileNameGen+" "+OutputFolder)
+#        codeLines2.append("rm -rf "+tmpFolder)
+
+        for codeLine in codeLines2:
+            submitfile2.write(codeLine+" \n")
+
+        submitfile2.write("rm "+ submitFileNameSH + " \n")
+	submitfile2.write("rm -r "+ tmp + " \n")
+        submitfile2.close()
+
+        cmd = "chmod a+x " + submitFileNameSH
+        os.system(cmd)
+        cmd = "qsub -e /dev/null -o /dev/null "+ submitFileNameSH
+        os.system(cmd)
+        
+        return True
+
+    else:
+        return False
+
+#initRun()
+#for i in range(100):
+optionsFile = open("options.in", 'r')
+options = optionsFile.read().split("\n")
+os.system("source /afs/ipp-garching.mpg.de/home/l/lscyboz/Herwig-7.0.3/src/Rivet-2.4.0/rivetenv.sh")
+os.system("export RIVET_ANALYSIS_PATH=/afs/ipp-garching.mpg.de/home/l/lscyboz/RivetCustomAnalyses/:$RIVET_ANALYSIS_PATH")
+
+for orders in options[0].split("\t"):
+
+  order = orders
+  for e,energies in enumerate(options[1].split("\t")):
+
+    Ecm=energies
+    for scales in options[2].split("\t"):
+
+        scale=scales
+        for pdfs in options[3].split("\t"):
+
+          pdf=pdfs
+          for showers in options[4].split("\t"):
+
+                shower=showers
+		settings=order+"_"+Ecm+"_"+scale+"_"+pdf+"_"+shower
+		if order=="LO":
+                          InputFolder="/afs/ipp-garching.mpg.de/home/l/lscyboz/GenericLO/"
+                elif order=="NLO":
+                          InputFolder="/afs/ipp-garching.mpg.de/home/l/lscyboz/Generic/"
+		index=3*e+6
+
+		flag=False
+		for i in range(nRuns):
+			spec='%03.0f' % (i,)
+			sampledPars = "/afs/ipp-garching.mpg.de/home/l/lscyboz/MC_Herwig_"+settings+"/"
+			if not os.path.exists(sampledPars+spec):
+			  os.system("mkdir -p "+sampledPars+spec)
+			os.system("cp "+InputFolder+"Herwig_"+settings+".in "+sampledPars)
+			SubmitHerwigJob(nEvPerFile, i, "tT_matchbox_"+settings+".run", index)
+		while True:
+                  os.system('qstat -u lscyboz > file')
+                  strn=open('file', 'r').read()
+                  if strn=='': break
+                  time.sleep(10)
+		
+		for norms in options[index+1].split("\t"):
+		  if not os.path.exists(sampledPars+"MC_Herwig_"+settings+"_"+norms+".yoda") or flag==True:
+			os.system("yodamerge "+sampledPars+"*/*"+norms+".yoda -o "+sampledPars+"MC_Herwig_"+settings+"_"+norms+".yoda")
+		if not os.path.exists(sampledPars+"MC_Herwig_"+settings+"_unnorm.yoda") or flag==True:
+			os.system("yodamerge "+sampledPars+"*/*"+norms+".yoda -o "+sampledPars+"MC_Herwig_"+settings+"_unnorm.yoda")
