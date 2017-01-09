73ca44e3464b59ca32c1a192f92f754f7306ae63
Ludovic Michel Scyboz <lscyboz@mppui2.t2.rzg.mpg.de>
2017-01-09 15:42:30 +0100

    Last version: diff with preexisting?

--- Herwig7Generate_dilepton.py
+++ Herwig7Generate_dilepton.py
@@ -121,7 +121,7 @@ def SubmitHerwigJob(nEvents, seed, alphaSMZ, InputFileNameGen, index):
 
         cmd = "chmod a+x " + submitFileNameSH
         os.system(cmd)
-        cmd = "qsub -l h_rt=05:00:00 "+ submitFileNameSH
+        cmd = "qsub -l "+ submitFileNameSH
         os.system(cmd)
 
         return True
@@ -194,7 +194,7 @@ for subdir in SubDirPath(pars):
 
 	                ## If no control (number of runs, right number of events...)
 	                ## is needed, just control if one of the final yoda files exists.
-	                breakLoop = (newControl == True) and ((order==ControlIndex) or Ecm==EnergyIndex or (alphaSMZ!="1.383333e-01"))
+	                breakLoop = (newControl == True) and ((order==ControlIndex) or Ecm==EnergyIndex or (alphaSMZ!="1.316667e-01"))
 	                if os.path.exists(sampledPars+"MC_Herwig_"+settings+"_"+options[index+1].split("\t")[0]+".yoda") and newControl == False or breakLoop: break
 	                if order=="LO":
 	                          InputFolder="/afs/ipp-garching.mpg.de/home/l/lscyboz/GenericLO/"
